14b6b2ce77d0b2072e5ec51c0c97dc543a35055a
Haris Saran <hsaran100@academic.rrc.ca>
2021-02-02 16:50:34 -0600

    Changed the text from Notes to SIGN

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -5,8 +5,8 @@ export default function Home() {
   return (
     <div className="Home">
       <div className="lander">
-        <h1>Scratch</h1>
-        <p className="text-muted">A simple note taking app</p>
+        <h1>SIGN</h1>
+        <p className="text-muted">Help Us, Help You</p>
       </div>
     </div>
   );
